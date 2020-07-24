2c5e3d9e6e14547716f4d5eb33a16571ea2ee1fa
Gennadiy Sabinin <gsabinin@icloud.com>
2020-07-24 20:54:28 +0300

    PHP_1

--- 6.php
+++ 6.php
@@ -1,16 +1,19 @@
 <?php
-// что то я сам алгоритм не понял...
-// а как работает рекурсия вроде понятно
-function power($val, $pow,$val2=0){
-    $val2=$val;
-    if($pow == 1){
-        echo $val;
+
+function power($val, $pow,$val2=1){
+    //echo $val2;
+    if($pow==0){
+        echo 1;
         return $val;
     }
-    $val=$val*$val2;
+    if($pow == 1){
+        echo $val2*=$val;
+        return $val;
+    }
+    $val2 *= $val;
     power($val, $pow-1, $val2);
 }
- power(2,3);
+ power(12,0);
  //echo pow(2,3);
 
 ?>
