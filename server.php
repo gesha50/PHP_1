cef121c22d852d46adda9c2116a73fb7e9c7041d
Gennadiy Sabinin <gsabinin@icloud.com>
2020-08-05 16:38:39 +0300

    PHP_1

--- server.php
+++ server.php
@@ -1,11 +1,10 @@
 <?php
-    //print_r($_GET[name]);
     $foto = $_GET[name];
-    $path = $_SERVER['DOCUMENT_ROOT'].'/minimalistica/big/'.$foto;
-    //print_r($path);
-    //fopen($path);
-    Header("Content-type: image/jpeg");
-    imagejpeg($path);
-
-
+    $path = 'big/'.$foto;
+    Header("location: ".$path);
 ?>
+    
+
+
+
+
